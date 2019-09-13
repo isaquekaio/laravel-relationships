ebc8cc7b4963ee9b052cad45f61f32db6abdc52d
Isaque Kaio <isaque.kaio@lais.huol.ufrn.br>
2019-09-13 14:23:55 -0300

    Criando tabelas, models e controller para o  relacionamento 1 para muitos.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+//One to One
+
 $this->get('countries', 'OneToOneController@index');
 $this->get('one-to-one', 'OneToOneController@oneToOne');
 $this->get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 $this->get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
+
+//One to Many
+
+$this->get('one-to-many', 'OneToManyController@OneToMany');
 
 Route::get('/', function () {
     return view('welcome');
